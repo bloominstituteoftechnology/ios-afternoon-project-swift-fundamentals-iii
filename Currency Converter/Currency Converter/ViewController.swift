9ee9270c75e950504100f1952618392af7936e4c
Jake Connerly <jakeconnerly@Macintosh.local>
2019-06-03 14:57:00 -0500

    Inital Commit

---
+++ Currency Converter/Currency Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Jake Connerly on 6/3/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
