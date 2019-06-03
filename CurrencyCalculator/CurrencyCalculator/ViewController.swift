fb6c70f1f81b5d381ded20c59ea1c1bcbc4fb405
Alex Shillingford <alexshillingford@Alexs-MacBook-Pro-2.local>
2019-06-03 15:13:38 -0600

    Constraints, IBOutlets, and IBActions set up

--- CurrencyCalculator/CurrencyCalculator/ViewController.swift
+++ CurrencyCalculator/CurrencyCalculator/ViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fromCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyLabel: UILabel!
+    @IBOutlet weak var cadButton: UIButton!
+    @IBOutlet weak var pesoButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func convertButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cadButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pesoButtonPressed(_ sender: Any) {
+        
+    }
+    
 
 
 }
