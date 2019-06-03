4a9a1d1800f7266fcefca1deebfbe1699a706f12
Alex Shillingford <alexshillingford@Alexs-MacBook-Pro-2.local>
2019-06-03 16:33:55 -0600

    Working calculator with currency formatting

--- CurrencyCalculator/CurrencyCalculator/ViewController.swift
+++ CurrencyCalculator/CurrencyCalculator/ViewController.swift
@@ -16,22 +16,66 @@ class ViewController: UIViewController {
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var pesoButton: UIButton!
     
+    var currencyType: CurrencyType = .cad
+    
+    var currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+    
+    enum CurrencyType {
+        case cad
+        case peso
+    }
+    
+    func convert(dollars: Double, to unit: CurrencyType) -> Double {
+        let cad = dollars * 1.35
+        let peso = dollars * 19.78
+        var outcome: Double
+        
+        
+        switch unit {
+        case .peso:
+            outcome = peso
+        default:
+            outcome = cad
+        }
+        
+        return outcome
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cadButton.isSelected.toggle()
+        pesoButton.isSelected = false
     }
     
     @IBAction func convertButtonPressed(_ sender: Any) {
+        guard let userInput = fromCurrencyTextField.text else {
+            toCurrencyTextField.text = "Invalid number entered"
+            return
+        }
         
+        toCurrencyTextField.text = currencyFormatter.string(for: convert(dollars: Double(userInput) ?? 0.00, to: currencyType))
     }
     
     @IBAction func cadButtonPressed(_ sender: Any) {
+        currencyType = .cad
+        cadButton.isSelected.toggle()
+        pesoButton.isSelected = false
         
+        toCurrencyLabel.text = "Currency (CAD):"
     }
     
     @IBAction func pesoButtonPressed(_ sender: Any) {
+        currencyType = .peso
+        pesoButton.isSelected.toggle()
+        cadButton.isSelected = false
+        
+        toCurrencyLabel.text = "Currency (Peso):"
         
     }
     
