10b68a770165baed4134504d0da09fab151fb8ce
Jordan Skenandore <jordan.skenandore@gmail.com>
2019-07-15 21:57:00 -0600

    Created Xcode Project

---
+++ CurrencyCalculator/CurrencyCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyCalculator
+//
+//  Created by Lambda_School_Loaner_141 on 7/15/19.
+//  Copyright © 2019 Lambda_School_Loaner_141. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
