225f541e907f650757dad3865a1f823dfdb3b4c7
Jessie <threedaystoryllc@icloud.com>
2019-07-15 23:48:55 -0400

    Created Xcode Project

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Jessie Ann Griffin on 7/15/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
