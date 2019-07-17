4097c3b4d5dd86b37dec659f6d2ccc13c49101a8
Jordan Skenandore <jordan.skenandore@gmail.com>
2019-07-16 23:58:48 -0600

    Finished Project

--- CurrencyCalculator/CurrencyCalculator/ViewController.swift
+++ CurrencyCalculator/CurrencyCalculator/ViewController.swift
@@ -31,25 +31,61 @@ class ViewController: UIViewController {
     }
     
     enum CurrencyType {
-        case pad
+        case cad
         case peso
     }
     
-    var currencyType = CurrencyType.self
+    var currencyType: CurrencyType = .cad
     
+    func convert(_ dollars: Double, to unit: CurrencyType) -> Double {
+        var convertedAmount: Double
+        if unit == .cad {
+            convertedAmount = dollars * 1.35
+        } else {
+            convertedAmount = dollars * 19.38
+        }
+        return convertedAmount
     }
     
+    
     @IBAction func convertButtonPressed(_ sender: Any) {
+        guard let fromCurrencyString = fromCurrencyTextField.text else {return}
+        
+        guard let fromCurrency = Double(fromCurrencyString) else {
+            print("Invalid number")
+            return
+        }
+        var convertedAmount: Double
+        if cadButton.isSelected {
+            convertedAmount = convert(fromCurrency, to: .cad)
+            toCurrencyLabel.text = "Currency (CAD)"
+        } else {
+            convertedAmount = convert(fromCurrency, to: .peso)
+            toCurrencyLabel.text = "Currency (Peso)"
+            pesoButton.isSelected = true
+        }
+        
+        toCurrencyTextField.text = "\(convertedAmount)"
     }
     
     
     @IBAction func cadButtonPressed(_ sender: Any) {
+        
+        guard let button = sender as? UIButton else {return}
+        
+        button.isSelected.toggle()
+        pesoButton.isSelected = false
+        
     }
     
     
     @IBAction func pesoButtonPressed(_ sender: Any) {
+        
+        guard let button = sender as? UIButton else {return}
+        
+        button.isSelected.toggle()
+        cadButton.isSelected = false
     }
-
 
 
 }
