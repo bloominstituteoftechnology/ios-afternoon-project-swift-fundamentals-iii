955a1c6d079f12ecece2929c3518fbe7c7d6de08
Joseph Rogers <Joerogersss2016@gmail.com>
2019-07-17 17:09:24 -0500

    Final Project

--- FundamentalsIII- CurrencyConverter/FundamentalsIII- CurrencyConverter/ViewController.swift
+++ FundamentalsIII- CurrencyConverter/FundamentalsIII- CurrencyConverter/ViewController.swift
@@ -19,23 +19,54 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyType = .cad
+        cadButton.isSelected = true
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    var currencyType: CurrencyType?
     enum CurrencyType {
         case cad
         case peso
     }
     
-    var currencyType = CurrencyType.cad
+    
+    
     
     @IBAction func convertButtonPressed(_ sender: Any) {
+        guard let dollars: Double = Double(fromCurrencyTextField.text!),
+            let currencyType: CurrencyType = currencyType else {
+                return }
+        
+        let currencyFormat = NumberFormatter()
+        currencyFormat.numberStyle = .currency
+        currencyFormat.locale = Locale(identifier: "en_US")
+        
+        toCurrencyTextField.text = currencyFormat.string(from: NSNumber(value: convert(dollars: dollars, to: currencyType)))
     }
-    
     @IBAction func cadButtonPressed(_ sender: Any) {
+        currencyType = .cad
+        cadButton.isSelected = true
+        pseoButton.isSelected = false
+        toCurrencyLabel.text = "Currency (CAD)"
     }
-    
     @IBAction func pesoButtonPressed(_ sender: Any) {
+        currencyType = .peso
+        pseoButton.isSelected = true
+        cadButton.isSelected = false
+        toCurrencyLabel.text = "Currency (Peso)"
+    }
+
+
+func convert(dollars: Double, to unit: CurrencyType) -> Double {
+    let cadPrice: Double = 1.31
+    let pesoPrice: Double = 19.10
+    switch unit {
+    case .cad:
+        return dollars * cadPrice
+    case .peso:
+        return dollars * pesoPrice
     }
 }
-
+}
