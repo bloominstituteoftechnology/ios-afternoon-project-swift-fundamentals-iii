40e0bcff50dda87c351fa12279dbc0e0df33686a
Jordan Skenandore <jordan.skenandore@gmail.com>
2019-07-16 21:47:47 -0600

    Created outlets and actions for storyboard

--- CurrencyCalculator/CurrencyCalculator/ViewController.swift
+++ CurrencyCalculator/CurrencyCalculator/ViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var fromCurrencyTextField: UITextField!
+    
+    
+    @IBOutlet weak var toCurrencyTextField: UITextField!
+    
+    
+    @IBOutlet weak var toCurrencyLabel: UILabel!
+    
+    
+    @IBOutlet weak var cadButton: UIButton!
+    
+    
+    @IBOutlet weak var pesoButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    enum CurrencyType {
+        case pad
+        case peso
+    }
+    
+    var currencyType = CurrencyType.self
+    
+    }
+    
+    @IBAction func convertButtonPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func cadButtonPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func pesoButtonPressed(_ sender: Any) {
+    }
+
 
 
 }
