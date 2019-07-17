50727abe88a729d7d2691e6d166376887dc2041d
John Kouris <jdkouris@gmail.com>
2019-07-16 18:10:41 -0700

    Completed the MVP requirements and the stretch goal. Also added made some UI changes by adding a title, changing the font family to Avenir-Next, and increasing the font sizes for all UI elements

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by John Kouris on 7/15/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    enum CurrencyType {
+        case cad
+        case peso
+    }
+    
+    var currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+    
+    @IBOutlet weak var fromCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyLabel: UILabel!
+    @IBOutlet weak var cadButton: UIButton!
+    @IBOutlet weak var pesoButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cadButton.isSelected = true
+    }
+    
+    func convert(dollars: Double, to unit: CurrencyType) -> String {
+        
+        var totalAmount = 0.0
+        
+        if unit == .cad {
+            totalAmount = dollars * 1.31
+        } else if unit == .peso {
+            totalAmount = dollars * 18.98
+        }
+        
+        if let formattedCurrencyTotal = currencyFormatter.string(from: NSNumber(value: totalAmount)) {
+            return formattedCurrencyTotal
+        } else {
+            return ""
+        }
+        
+    }
+
+    @IBAction func convertButtonPressed(_ sender: Any) {
+        
+        guard let currencyString = fromCurrencyTextField.text else { return }
+        
+        guard let currency = Double(currencyString) else { return }
+        
+        if cadButton.isSelected {
+            toCurrencyLabel.text = "Currency (CAD)"
+            toCurrencyTextField.text = String(convert(dollars: currency, to: .cad))
+        } else if pesoButton.isSelected {
+            toCurrencyLabel.text = "Currency (Peso)"
+            toCurrencyTextField.text = String(convert(dollars: currency, to: .peso))
+        }
+        
+    }
+    
+    @IBAction func cadButtonPressed(_ sender: Any) {
+        
+        guard let button = sender as? UIButton else { return }
+        
+        button.isSelected.toggle()
+        
+        pesoButton.isSelected = false
+        
+    }
+    
+    @IBAction func pesoButtonPressed(_ sender: Any) {
+        
+        guard let button = sender as? UIButton else { return }
+        
+        button.isSelected.toggle()
+        
+        cadButton.isSelected = false
+        
+    }
+    
+}
+
