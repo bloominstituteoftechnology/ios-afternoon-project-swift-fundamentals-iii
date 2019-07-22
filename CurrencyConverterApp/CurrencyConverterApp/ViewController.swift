e111857d9d0a8714e2d99a13a7cd29af01465169
swiftalicious <amazinggrace1@protonmail.com>
2019-07-22 13:03:57 -0700

    Set up storyboard view

---
+++ CurrencyConverterApp/CurrencyConverterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverterApp
+//
+//  Created by Jason on 7/22/19.
+//  Copyright © 2019 Jason Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
