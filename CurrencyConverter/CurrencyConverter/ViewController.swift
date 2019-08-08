8f3718737269edcf98d35a5468131be973fe29cc
ryandutson <mr.dutson@gmail.com>
2019-08-08 15:11:37 -0600

    Initial commit.

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Ben Gohlke on 8/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets/Properties
+    
+    @IBOutlet weak var fromCurrencyTextField: UITextField!
+    @IBOutlet weak var toCurrencyTextField: UITextField!
+    
+    @IBOutlet weak var toCurrencyLabel: UILabel!
+    
+    @IBOutlet weak var cadButton: UIButton!
+    @IBOutlet weak var mxnButton: UIButton!
+
+    // MARK: - Actions
+    
+    @IBAction func convertButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func cadButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - Helper Methods
+}
+
