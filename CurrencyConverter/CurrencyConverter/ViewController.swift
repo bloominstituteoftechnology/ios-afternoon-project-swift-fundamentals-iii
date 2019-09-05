af46300412e77474d675ad6b75c086de6f22fe86
Niranjan Kumar <niranjankumar@Niranjans-MacBook-Pro-New.local>
2019-09-05 16:51:37 -0700

    initial commit

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
+var currency: Currency = .cad
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -24,16 +31,45 @@ class ViewController: UIViewController {
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
         
+        guard let currencyLabelString = toCurrencyLabel.text else {
+            return
+        }
+        guard let fromCurrency = Double(currencyLabelString) else {
+            print("N/A")
+            return
+        }
+        
     }
     
+    
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
         
+        if cadButton.isSelected {
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        } else {
+            currency = .mxn
+            toCurrencyLabel.text = "Currency (MXN)"
+        }
+        
+            
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
         
+        if cadButton.isSelected {
+
     }
     
     // MARK: - Helper Methods
-}
 
+    func convert(_ dollars: Double) -> Double {
+        
+    }
+    
+    
+    }
+}
+//test
