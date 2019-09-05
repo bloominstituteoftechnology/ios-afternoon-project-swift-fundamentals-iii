002bacfeb6d8cb730c7697f86c44eb813e6db67b
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-09-05 15:52:09 -0700

    initial commit

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+var currency: Currency?
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -23,17 +29,59 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        guard let dollars = fromCurrencyTextField.text,
+        let dollarString = Double(dollars) else {
+            return
+        }
+        let usDollars = convertUSD(dollarString)
+        toCurrencyTextField.text = ("$ \(usDollars)")
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
-        
+        currency = .cad
+        cadButton.isSelected = true
+        mxnButton.isSelected = false
+        toCurrencyLabel.text = "Currency (CAD)"
+    
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
-        
+        currency = .mxn
+        cadButton.isSelected = false
+        mxnButton.isSelected = true
+        toCurrencyLabel.text = "Currency (MXN)"
     }
     
     // MARK: - Helper Methods
-}
+//    func convert(_dollars: Double) -> Double {
+//        return
+//    }
 
+//    func convertMXN(_dollars: Double, to unit: Currency) -> Double {
+//        let currencyInPesos = _dollars * 17.42
+//        let currencyInCAD = _dollars * 1.17
+//
+////        switch unit {
+////        case .cad:
+////            return _dollars * currencyInCAD
+////        case .mxn:
+////            return _dollars * currencyInPesos
+////        }
+////    }
+////
+
+    func convertUSD(_ dollars: Double) -> Double {
+        if currency == .cad {
+            let cadRate = 1.17
+            let currencyInCAD = cadRate * dollars
+            return currencyInCAD
+        }
+        else {
+            let mxnRate = 17.42
+            let currencyInPesos = mxnRate * dollars
+            return currencyInPesos
+        }
+
+
+}
+}
