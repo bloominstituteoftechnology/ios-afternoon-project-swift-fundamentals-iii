87b8797eb4b233305cf2135c509472bfa509d8d0
Dennis Rudolph <D@Denniss-MacBook-Pro.local>
2019-09-05 18:28:24 -0400

    Completed Project

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
+    
+    var currency: Currency = .cad
     
     // MARK: - Outlets/Properties
     
@@ -17,23 +24,62 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var toCurrencyLabel: UILabel!
     
+    
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var mxnButton: UIButton!
 
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let fromCurrencyString = fromCurrencyTextField.text else {
+            return
+        }
+        guard let fromCurrency = Double(fromCurrencyString) else {
+            print("Invalid number")
+            return
+        }
         
+    var finalAnswer = convert(fromCurrency)
+    
+    toCurrencyTextField.text = String(finalAnswer)
+   
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+        
+        if cadButton.isSelected {
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
+        
         
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        cadButton.isSelected.toggle()
+        
+        if mxnButton.isSelected {
+            currency = .mxn
+            toCurrencyLabel.text = "Currency (MXN)"
+        }
         
     }
     
     // MARK: - Helper Methods
+    
+    func convert(_ dollars: Double) -> Double {
+        let answer: Double
+        
+        if currency == .cad {
+            answer = dollars * 1.33
+        } else {
+            answer = dollars * 19.7
+        }
+        return answer
+    }
+    
 }
 
