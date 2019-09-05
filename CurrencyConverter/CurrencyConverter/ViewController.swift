e3ca49b51196f3e5f293138b414a0a59865539bc
Isvvc <lyons@tuta.io>
2019-09-05 13:40:56 -0600

    Complete project

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
+var currency: Currency = .cad
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -23,17 +30,58 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let fromCurrencyString = fromCurrencyTextField.text else {
+            return
+        }
         
+        guard let fromCurrency = Double(fromCurrencyString) else {
+            print("Invalid number")
+            return
+        }
+        
+        let toCurrency = convert(fromCurrency)
+        
+        toCurrencyTextField.text = String(toCurrency)
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
-        
+        sender.isSelected.toggle()
+        if sender.isSelected {
+            mxnButton.isSelected = false
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        } else {
+            mxnButton.isSelected = true
+            currency = .mxn
+            toCurrencyLabel.text = "Currency (MXN)"
+        }
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
-        
+        sender.isSelected.toggle()
+        if sender.isSelected {
+            cadButton.isSelected = false
+            currency = .mxn
+            toCurrencyLabel.text = "Currency (MXN)"
+        } else {
+            cadButton.isSelected = true
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
     }
     
     // MARK: - Helper Methods
+    
+    func convert(_ dollars: Double) -> Double {
+        var converted: Double
+        switch currency {
+        case .cad:
+            converted = dollars * 1.33
+        case .mxn:
+            converted = dollars * 19.70
+        }
+        
+        return converted
+    }
 }
 
