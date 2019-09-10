5305e2f0964337fb0daf12ba507ebb7e49bdb721
Blake Andrew Price <blakeandrewprice@gmail.com>
2019-09-10 15:05:31 -0700

    Finished Currency Converter Project

---
+++ iOS-Afternoon-Project-Swift-Fundamentals-III-Currency-ConverterTests/iOS_Afternoon_Project_Swift_Fundamentals_III_Currency_ConverterTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Afternoon_Project_Swift_Fundamentals_III_Currency_ConverterTests.swift
+//  iOS-Afternoon-Project-Swift-Fundamentals-III-Currency-ConverterTests
+//
+//  Created by Blake Andrew Price on 9/10/19.
+//  Copyright © 2019 Blake Andrew Price. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Afternoon_Project_Swift_Fundamentals_III_Currency_Converter
+
+class iOS_Afternoon_Project_Swift_Fundamentals_III_Currency_ConverterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
