032960cf9a5674f04ed9ffe72a74e0b713a9952a
Chris Price <chrisprice@Chriss-Mac-mini.fios-router.home>
2019-09-11 00:34:27 -0400

    MVP..

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
+    
+    var currency: Currency = .cad
     
     @IBOutlet weak var fromCurrencyTextField: UITextField!
     @IBOutlet weak var toCurrencyTextField: UITextField!
@@ -23,17 +30,61 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let fromCurrencyString = fromCurrencyTextField.text else {
+            return
+        }
+        
+        guard let currencyInputNumber = Double(fromCurrencyString) else {
+            print("Invalid number")
+            return
+        }
+        
+        toCurrencyTextField.text = String(convert(currencyInputNumber))
+        
         
     }
     
-    @IBAction func cadButtonTapped(_ sender: UIButton) {
+    @IBAction func cadButtonTapped(_ sender: Any) {
+        guard let button = sender as? UIButton else {
+            return
+        }
         
+        button.isSelected.toggle()
+        if button.isSelected {
+            currency = .cad
+            toCurrencyLabel.text = "Currency(CAD)"
+            mxnButton.isSelected.toggle()
+        } else {
+            return
+        }
     }
     
-    @IBAction func mxnButtonTapped(_ sender: UIButton) {
+    @IBAction func mxnButtonTapped(_ sender: Any) {
+        guard let button = sender as? UIButton else {
+            return
+        }
         
+        button.isSelected.toggle()
+        if button.isSelected {
+            currency = .mxn
+            toCurrencyLabel.text = "Currency(MXN)"
+            cadButton.isSelected.toggle()
+        } else {
+            return
+        }
     }
     
     // MARK: - Helper Methods
+    func convert(_ dollar: Double) -> Double {
+        var convertedValue: Double = 0.0
+        if currency == .cad {
+            convertedValue = dollar * 1.31
+        } else if currency == .mxn {
+            convertedValue = dollar * 19.5
+        }
+        return convertedValue
+    }
+    
 }
 
+//I'm doing tonight's project. I'm on numbers 3 and 4. I'm trying to toggle the one button off when the other is selected, but I am not sure how to access the
