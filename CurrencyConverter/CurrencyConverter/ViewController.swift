13dfb697ecd479b32f9d577361003f91c2f545d8
Christian Lorenzo <clbmiami2004@gmail.com>
2019-09-11 21:08:26 -0700

    Added a currency enum to the code.

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -22,6 +22,13 @@ class ViewController: UIViewController {
 
     // MARK: - Actions
     
+    
+    
+    enum Currency {
+        case Mxn
+        case Cnd
+    }
+    
     @IBAction func convertButtonTapped(_ sender: UIButton) {
         
     }
