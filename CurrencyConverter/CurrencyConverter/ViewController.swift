8adb75484725f970cdf15a5b2a514612eb785367
Zack Larsen <zacklarsen1@gmail.com>
2019-10-31 17:59:22 -0600

    finished assignment

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -14,7 +14,8 @@ enum Currency {
 }
 var currency = Currency.cad
 
-
+let cadConverter = 1.33
+let mxnConverter = 19.70
 
 
 
@@ -33,14 +34,35 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        guard let
+        guard let usd = fromCurrencyTextField.text else {
+        return
+        }
+        
+        let dollars = Double(usd) ?? 0
+        
+//        Call currency converter
+        let convertedDollars = convert(dollars)
+        toCurrencyTextField.text = "$\(convertedDollars)"
+        
+        
+        
+        
+            
+        
+        
+        
+        
         
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+//        sender.isSelected.toggle()
         cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
         
         toCurrencyLabel.text = "Currency (CAD)"
+        
+        currency = .cad
         
             
         }
@@ -49,31 +71,28 @@ class ViewController: UIViewController {
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
         mxnButton.isSelected.toggle()
+        cadButton.isSelected.toggle()
+        
         
         toCurrencyLabel.text = "Currency (MXN)"
-        
+        currency = .mxn
         
     }
 
 
     
     // MARK: - Helper Methods
-}
-func convertCad(_ dollars: Double) -> Double {
-    let cad = dollars * 1.33
-    return cad
-}
 
-func convertMXN(_ dollars: Double) -> Double {
-    let mxn = dollars * 19.24
-    return mxn
-}
-    func convert(_ dollars: Double) -> Double {
-        if currency == Currency.cad {
-            return convertCad(dollars)
-        } else {
-            return convertMXN(dollars)
-        
+
+    func convert(_ dollars: Double) -> Double
+    {
+        switch currency {
+        case .cad:
+            return dollars * cadConverter
+        case .mxn:
+            return dollars * mxnConverter
         }
     }
+        
+}
 
