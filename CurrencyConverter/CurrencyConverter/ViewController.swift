5c17449293aed387691ae5fb7ac950267c54bf88
Troy Christian <apostasy8@gmail.com>
2019-10-31 20:58:17 -0400

    Complete Project, currency converter functional.

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -28,15 +28,27 @@ class ViewController: UIViewController {
 
     // MARK: - Actions
     
-    @IBAction func convertButtonTapped(_ sender: UIButton) {
+    @IBAction func convertButtonTapped(_ sender: UIButton)  {
         
-    }
+       
+        guard let capture = fromCurrencyTextField.text else{return}
+        let captureInput = Double(capture) ?? 0.0
+       
+        toCurrencyTextField.text = String(convert(captureInput))
+            
+        }
+    
+       
+        
+        
+
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
         cadButton.isSelected.toggle()
         mxnButton.isSelected = false
         if cadButton.isSelected{
             toCurrencyLabel.text = "Currency(CAD)"
+            currency = .cad
     
         }}
         
@@ -52,13 +64,26 @@ class ViewController: UIViewController {
         cadButton.isSelected = false
         if mxnButton.isSelected{
             toCurrencyLabel.text = "Currency(MXN)"
+            currency = .mxn
         }
         }
-        }
+        
         
         
     
 
+    
     // MARK: - Helper Methods
-
-
+func convert(_ dollars:Double) -> Double {
+    var currencyConverted: Double
+    
+    if cadButton.isSelected == true{
+        currencyConverted = dollars * 1.32
+        return currencyConverted
+    } else {
+        currencyConverted = dollars * 19.23
+    }
+    return currencyConverted
+    
+}
+}
