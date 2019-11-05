a01223ea2150a8945877ca33c432cbb2d4e760ff
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2019-11-05 02:51:36 +0100

    ...

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -19,21 +24,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var mxnButton: UIButton!
+    
+    var currency: Currency = .cad
 
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
         
+        guard let fromCurrencyString = fromCurrencyTextField.text
+            else { return }
+        guard Double(fromCurrencyString) != nil
+            else {
+            print("Invalid number. Unable to initialize a double from the string.")
+            return
+        }
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+        var currency:  Currency = .cad
         
-    }
+        }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+         mxnButton.isSelected.toggle()
+         cadButton.isSelected.toggle()
         
-    }
+        }
     
     // MARK: - Helper Methods
-}
 
+
+
+    func convertToCad(amount: Double) -> Double {
+        var currency: Double
+        
+        if cadButton.isSelected {
+            currency * 1.32
+        } else {
+            currency * 19.23
+        }
+    }
+}
