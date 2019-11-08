f30f0d311bdea598f9196733b6fbcc7353f0b532
David Wright <davidtwright@gmail.com>
2019-11-07 22:26:45 -0500

    Completed Steps 1-7 of the CurrencyConverter project.

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
+    
+    var currency = Currency.cad
     
     @IBOutlet weak var fromCurrencyTextField: UITextField!
     @IBOutlet weak var toCurrencyTextField: UITextField!
@@ -23,17 +30,41 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        guard let usd = Double(fromCurrencyTextField.text!) else { return }
+        toCurrencyTextField.text = String(convert(usd))
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
         
+        if cadButton.isSelected {
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
         
+        if mxnButton.isSelected {
+            currency = .mxn
+            toCurrencyLabel.text = "Currency (MXN)"
+        }
     }
     
     // MARK: - Helper Methods
+    
+    func convert(_ dollars: Double) -> Double {
+        if currency == .cad {
+            return dollars * 1.33
+        } else {
+            return dollars * 19.7
+        }
+    }
+    
 }
 
+//$1 USD to $1.33 CAD
+//$1 USD to $19.70 MXN
