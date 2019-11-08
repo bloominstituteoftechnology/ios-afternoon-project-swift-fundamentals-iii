37b9ec32dccace5f2bee6e78118af0e1308d1780
morgan smith <mssmitty07@gmail.com>
2019-11-07 22:50:54 -0500

    Commiting currency converter project

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
+var currency: Currency = .cad
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -19,21 +26,60 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var mxnButton: UIButton!
-
+    
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        guard let fromCurrency = fromCurrencyTextField.text,
+            let currencyUSD = Double(fromCurrency) else {return}
+       let convertedCurrency = convert(currencyUSD)
+        toCurrencyTextField.text = "\(convertedCurrency)"
     }
+    
+    var currencyCad: Double = 0
+    var currencyMxn: Double = 0
+    
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
         
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+        
+        if cadButton.isSelected {
+            currency = .cad
+            self.toCurrencyLabel.text = "Currency (CAD)"
+        } else {
+            currency = .mxn
+            self.toCurrencyLabel.text = "Currency (MXN)"
+        }
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
         
+        mxnButton.isSelected.toggle()
+        cadButton.isSelected.toggle()
+        
+        if mxnButton.isSelected {
+            currency = .mxn
+            self.toCurrencyLabel.text = "Currency (MXN)"
+        } else {
+            currency = .cad
+            self.toCurrencyLabel.text = "Currency (CAD)"
+        }
     }
     
+    
     // MARK: - Helper Methods
+    
+    func convert(_ dollars: Double) -> Double {
+        if cadButton.isSelected {
+            currencyCad = dollars * 1.32
+            return currencyCad
+        } else {
+            currencyMxn = dollars * 19.14
+            return currencyMxn
+        }
+    }
 }
+
 
