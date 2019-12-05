5365b9942a1d233a5979f3cff0cd91f2aaf1d754
Michael McGrath <maybemichael@icloud.com>
2019-12-05 15:23:52 -0800

    Afternoon Project Finished

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,16 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
+var currency = Currency.cad
+
+
+
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -23,17 +33,59 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        guard let userDollarString = fromCurrencyTextField.text else {
+            return
+        }
+        guard let dollarTotal = Double(userDollarString) else {
+            print("Invalid dollar amount. Unable to initialize a double from a string.")
+            return
+        }
+        if mxnButton.isSelected == true {
+            toCurrencyTextField.text = convertToCurrency(amount: convert(dollarTotal))
+        } else if cadButton.isSelected == true {
+            toCurrencyTextField.text = convertToCurrency(amount: convert(dollarTotal))
+        }
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
-        
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+        if cadButton.isSelected == true {
+            currency = .cad
+        }
+        toCurrencyLabel.text = "Currency (CAD)"
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
-        
+        mxnButton.isSelected = !mxnButton.isSelected
+        cadButton.isSelected = !cadButton.isSelected
+        if mxnButton.isSelected == true {
+            currency = .mxn
+        }
+        toCurrencyLabel.text = "Currency (MXN)"
     }
     
     // MARK: - Helper Methods
+    
+    // On December 5th, 2019 conversion values are:
+    // $1 USD to $1.32 CAD
+    // $1 USD to $19.36 MXN
+    
+    func convert(_ dollars: Double) -> Double {
+        var conversionTotal: Double
+        
+        if currency == .cad {
+            conversionTotal = dollars * 1.32
+        } else {
+            conversionTotal = dollars * 19.36
+        }
+        return conversionTotal
+    }
+    
+    func convertToCurrency(amount: Double) -> String {
+            let currencyFormatter = NumberFormatter()
+            currencyFormatter.numberStyle = .currency
+        return currencyFormatter.string(from: NSNumber(value: amount))!
+        
+    }
 }
-
