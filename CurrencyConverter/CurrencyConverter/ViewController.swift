2ded1fe3dc05c07e4798c001c6d5782ee2712819
Tobi Tselios <tobi.tselios@gmail.com>
2019-12-05 21:17:10 +0000

    Steps 1-3 complete

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -15,9 +15,9 @@ enum Currency {
 
 class ViewController: UIViewController {
     
-    var currency: Currency = .cad
-    
     // MARK: - Outlets/Properties
+    
+    var currency: Currency = .cad
     
     @IBOutlet weak var fromCurrencyTextField: UITextField!
     @IBOutlet weak var toCurrencyTextField: UITextField!
@@ -30,22 +30,7 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        guard let userInput = fromCurrencyTextField.text else { return }
         
-        guard let convertedInput = Double(userInput) else {
-            print("This is not a valid input")
-            return
-        }
-        
-        var convertedCurrency: Double
-        
-        if cadButton.isSelected {
-            convertedCurrency = convert(convertedInput)
-            toCurrencyTextField.text = "\(userInput) US dollars in Canadian dollars is \(convertedCurrency)"
-        } else {
-            convertedCurrency = convert(convertedInput)
-            toCurrencyTextField.text = "\(userInput) US dollars in Mexican pesos is \(convertedCurrency)"
-        }
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
@@ -54,32 +39,15 @@ class ViewController: UIViewController {
         
         if cadButton.isSelected {
             currency = .cad
-            toCurrencyLabel.text = "Currency (CAD)"
+            toCurrencyLabel.text = "Currency(CAD)"
         }
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
-        mxnButton.isSelected.toggle()
-        cadButton.isSelected.toggle()
-        
-        if mxnButton.isSelected {
-            currency = .mxn
-            toCurrencyLabel.text = "Currency (MXN)"
-        }
+      
     }
     
     // MARK: - Helper Methods
     
-    func convert(_ dollars: Double) -> Double {
-        var convertedCurrency: Double
-        
-        if currency == .cad {
-            convertedCurrency = dollars * 1.32
-        } else {
-            convertedCurrency = dollars * 19.11
-        }
-        
-        return convertedCurrency
-    }
 }
 
