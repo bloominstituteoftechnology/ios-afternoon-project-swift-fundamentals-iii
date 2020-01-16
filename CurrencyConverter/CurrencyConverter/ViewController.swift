800bf303c41fd65e8b367f5c6b7fefe1600cc499
Scott Harris <scottharris86@me.com>
2020-01-16 18:53:41 -0500

    completed all objectives and the stretch goal

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
+    
+    var currency: Currency = .cad
+    var currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
     
     @IBOutlet weak var fromCurrencyTextField: UITextField!
     @IBOutlet weak var toCurrencyTextField: UITextField!
@@ -23,17 +35,48 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let convertedInput = Double(fromCurrencyTextField.text!) else { return }
+        let convertedMoney = convert(convertedInput)
+        toCurrencyTextField.text = currencyFormatter.string(from: NSNumber(value: convertedMoney))
+        
         
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        handleToCurrencySelection()
+        
+        if sender.isSelected {
+            currency = .cad
+            toCurrencyLabel.text = "Currency(CAD)"
+        }
+        
         
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        handleToCurrencySelection()
+        if sender.isSelected {
+            currency = .mxn
+            toCurrencyLabel.text = "Currency(MXN)"
+        }
         
     }
     
     // MARK: - Helper Methods
+    func convert(_ dollars: Double) -> Double {
+        switch currency {
+            case .cad:
+                return dollars * 1.33
+            case .mxn:
+                return dollars * 19.70
+        }
+    }
+    
+    func handleToCurrencySelection() {
+        mxnButton.isSelected.toggle()
+        cadButton.isSelected.toggle()
+        toCurrencyTextField.text = ""
+        
+    }
 }
 
