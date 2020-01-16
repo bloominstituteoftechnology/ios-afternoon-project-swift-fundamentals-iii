c1cf4bbb36ce843f9db051ae670446b7380f63bf
Chris Gonzales <57194564+supergonzo812@users.noreply.github.com>
2020-01-16 15:59:52 -0800

    Final Converter project

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-enum Currency {
-    case cad
-    case mxn
+enum Currency: Double {
+    case cad = 1.30
+    case mxn = 18.81
 }
 
 var currency: Currency = .cad
@@ -30,21 +30,54 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let text = fromCurrencyTextField.text,
+        let doubleAmount = Double(text) else{
+           return
+        }
         
+        let converTotal = doubleAmount * currency.rawValue
+        
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale.current
+        
+        guard let formatedTotal = currencyFormatter.string(from: NSNumber(value: converTotal)) else {
+            return
+        }
+        
+        toCurrencyTextField.text = formatedTotal
+
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
-        sender.isSelected = true
-        if sender.isSelected == true{
+        if cadButton.isSelected == true {
+              return
+            }
+        cadButton.isSelected = true
+        mxnButton.isSelected = false
+        
+        if cadButton.isSelected == true{
             currency = .cad}
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
-        sender.isSelected = false
-        if sender.isSelected == true{
+         if mxnButton.isSelected == true {
+                     return
+                   }
+            mxnButton.isSelected = true
+        cadButton.isSelected = false
+        
+        if mxnButton.isSelected == true{
         currency = .mxn}
     }
     
     // MARK: - Helper Methods
+    
+    func convert(_ dollars: Double) -> Double{
+        let convertTotal = dollars * currency.rawValue
+        print(convertTotal)
+        return convertTotal
+    }
 }
 
