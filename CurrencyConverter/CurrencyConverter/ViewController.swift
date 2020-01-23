6492454081922dc2f7d82492fe7356ed88288f77
Claudia Maciel <clc_80@yahoo.com>
2020-01-22 22:07:08 -0800

    project completed

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
+    
+    var currency: Currency = .cad
+    var location: String = "en_CA"
     
     // MARK: - Outlets/Properties
     
@@ -23,17 +31,67 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        //check to see if there is something entered
+        guard let stringDollars = fromCurrencyTextField.text else {return}
         
+        //check to see if it converts to a double
+        guard let dollars = Double(stringDollars) else {
+            print("Invalid number")
+            return
+        }
+        toCurrencyTextField.text = convertDoubleToCurrency(convert(dollars))
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        //toggle the buttons
+        toggleButtons()
         
+        //check to see if this button is selected and set to the currency to canadian
+        if cadButton.isSelected {
+            currency = .cad
+        }
+        
+        //change the text label
+        toCurrencyLabel.text = "Currency (CAD)"
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        //toggle the buttons
+        toggleButtons()
         
+        //check to see if the mxn button is selected and set the currency to mexican
+        if mxnButton.isSelected {
+            currency = .mxn
+        }
+        
+        //change the text label
+        toCurrencyLabel.text = "Currency (MXN)"
     }
     
     // MARK: - Helper Methods
+    func toggleButtons() {
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+    }
+    func convert(_ dollars: Double) -> Double {
+        switch currency {
+        case .cad:
+            location = "en_CA"
+            return dollars * 1.32
+        case .mxn:
+            location = "es_MX"
+            return dollars * 18.72
+        }
+    }
+    
+    func convertDoubleToCurrency(_ amount: Double) -> String {
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale(identifier: location)
+        
+        return numberFormatter.string(from: NSNumber(value: amount))!
+        
+    }
 }
 
