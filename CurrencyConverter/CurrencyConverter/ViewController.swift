324e16cb4b99ef3dbad479f42a22a79c1551e4d1
Christopher DeVito <toolofjesus@protonmail.com>
2020-01-23 00:43:13 -0500

    Followed the directions and added functionality to currencyconverter/ViewController.swift

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
+var currency: Currency = .cad
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -21,19 +28,63 @@ class ViewController: UIViewController {
     @IBOutlet weak var mxnButton: UIButton!
 
     // MARK: - Actions
+
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let amount = fromCurrencyTextField.text else { return }
         
+        let convertedAmount = convert(Double(amount) ?? 0)
+        
+        let currencyFormatter: NumberFormatter = {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .currency
+            if cadButton.isSelected {
+                formatter.locale = Locale(identifier: "en_CA")
+            } else {
+                formatter.locale = Locale(identifier: "es_MX")
+            }
+            return formatter
+        }()
+        
+//        if cadButton.isSelected {
+//            currencyFormatter.locale = Locale(identifier: "en_CA")
+//        } else {
+//            currencyFormatter.locale = Locale(identifier: "es_MX")
+//        }
+        
+        let displayAmount = currencyFormatter.string(for: convertedAmount) ?? "$0.00"
+        
+        toCurrencyTextField.text = "\(displayAmount)"
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        cadButton?.isSelected.toggle()
+        mxnButton?.isSelected.toggle()
         
+        if cadButton.isSelected {
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        cadButton?.isSelected.toggle()
+        mxnButton?.isSelected.toggle()
         
+        if mxnButton.isSelected {
+            currency = .mxn
+            toCurrencyLabel.text = "Currency (MXN)"
+        }
     }
     
     // MARK: - Helper Methods
+    
+    func convert(_ dollars: Double) -> Double {
+        if currency == .cad {
+            return dollars * 1.32
+        } else {
+            return dollars * 18.72
+        }
+    }
 }
 
