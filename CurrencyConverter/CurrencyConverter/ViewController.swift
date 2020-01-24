89a7601a67013da26a68436cbe46a10a578731c6
Chad Parker <chad@chadparker.com>
2020-01-23 20:37:44 -0800

    Project complete

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
+enum Currency: String {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
+
+    var currency: Currency = .cad
+    var currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
     
     // MARK: - Outlets/Properties
     
@@ -23,17 +35,44 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        guard let inputString = fromCurrencyTextField.text else { return }
+        guard let dollars = Double(inputString) else { return }
+        let convertedAmount = convert(dollars)
+        toCurrencyTextField.text = format(convertedAmount)
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
-        
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+        if cadButton.isSelected {
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
-        
+        mxnButton.isSelected.toggle()
+        cadButton.isSelected.toggle()
+        if mxnButton.isSelected {
+            currency = .mxn
+            toCurrencyLabel.text = "Currency (MXN)"
+        }
     }
     
     // MARK: - Helper Methods
+
+    func convert(_ dollars: Double) -> Double {
+        switch currency {
+        case .cad:
+            return dollars * 1.31
+        case .mxn:
+            return dollars * 18.70
+        }
+    }
+
+    func format(_ amount: Double) -> String {
+        currencyFormatter.currencyCode = currency.rawValue
+        return currencyFormatter.string(from: NSNumber(value: amount)) ?? ""
+    }
 }
 
