208c037a304c0bb432f4cb2890a148d201936c82
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-01-23 19:54:19 -0800

    Completed currency homework

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
+var currency: Currency = .cad
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -23,17 +30,60 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let amountEnteredString = fromCurrencyTextField.text else { return }
         
+        guard let amountEntered = Double(amountEnteredString) else {
+            print("Invalid entry..")
+            return
+        }
+        
+        let currencyConverted = convert(amountEntered)
+        
+        toCurrencyTextField.text = String(currencyConverted)
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        guard let button = sender as? UIButton else { return }
+        
+        if cadButton.isSelected {
+            currency = .cad
+        }
+        
+        toCurrencyLabel.text = "Currency (CAD)"
+        
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
         
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        guard let button = sender as? UIButton else { return }
+        
+        if mxnButton.isSelected {
+            currency = .mxn
+        }
+        
+        toCurrencyLabel.text = "Currency (MXN)"
+        
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
         
     }
     
     // MARK: - Helper Methods
+    
+    func convert(_ dollars: Double) -> Double {
+        var convertedValue:Double = 0
+        let cadValue = 1.31
+        let mxnValue = 18.77
+        
+        if currency == .cad {
+            convertedValue = dollars * cadValue
+        } else if currency == .mxn {
+            convertedValue = dollars * mxnValue
+        }
+        
+        return convertedValue
+    }
 }
 
