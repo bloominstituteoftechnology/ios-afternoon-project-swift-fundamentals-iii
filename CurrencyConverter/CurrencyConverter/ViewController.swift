715b6f812cfffcda21ec5cf6e965320bd3f9ace6
Jarren Campos <jarrenmcampos@gmail.com>
2020-01-25 22:32:49 -0800

    Finished Project

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+    
+}
+var currency: Currency? = .cad
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -23,17 +30,50 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        guard let userInputString = fromCurrencyTextField.text else { return }
+        guard var userInput = Double(userInputString)
+            else{
+                print("Invalid Number")
+                return
+        }
+        userInput = convert(userInput)
+        toCurrencyTextField.text = "\(userInput)"
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+//        guard let button = sender as? UIButton else { return }
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+        if (cadButton.isSelected == true){
+            currency = .cad
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
+        
         
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+//        guard let button = sender as? UIButton else { return }
+        mxnButton.isSelected.toggle()
+        cadButton.isSelected.toggle()
+        if (mxnButton.isSelected == true){
+            currency = .mxn
+            toCurrencyLabel.text = "Currency (MXN)"
+        }
         
     }
     
     // MARK: - Helper Methods
+    func convert(_ dollars: Double) -> Double {
+        if currency == .cad {
+            let answer = dollars * 1.31
+            return answer
+        }
+        else if currency == .mxn{
+            let answer = dollars * 18.78
+            return answer
+        } else { return 0 }
+    }
 }
+
 
