25f100bcce554a90845133deedb55a3c4ef59d6d
Shawn Gee <shawngee@gmail.com>
2020-02-11 17:45:56 -0500

    Completed core project

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
+enum Currency: String {
+    case cad = "CAD"
+    case mxn = "Peso"
+    
+    var conversionRate: Double {
+        switch self {
+        case .cad:
+            return 1.33
+        case .mxn:
+            return 19.7
+        }
+    }
+}
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -19,21 +33,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var mxnButton: UIButton!
+    
+    var currency = Currency.cad
 
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        guard let inputString = fromCurrencyTextField.text,
+            let inputAmount = Double(inputString) else {
+                return
+        }
+        toCurrencyTextField.text = "$\(convert(inputAmount))"
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
-        
+        toggleCurrency()
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
-        
+        toggleCurrency()
     }
     
     // MARK: - Helper Methods
+    
+    func toggleCurrency() {
+        cadButton.isSelected.toggle()
+        mxnButton.isSelected.toggle()
+        
+        currency = cadButton.isSelected ? .cad : .mxn
+        toCurrencyLabel.text = "Currency: (\(currency.rawValue))"
+        
+        // If toggling, we should also either clear the toCurrencyTextField re-convert
+        toCurrencyTextField.text = ""
+    }
+    
+    func convert(_ dollars: Double) -> Double {
+        dollars * currency.conversionRate
+    }
 }
 
