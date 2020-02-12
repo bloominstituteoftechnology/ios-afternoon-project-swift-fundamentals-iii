48d15b14f2f1f8dedbcab8ce1e288efa8df1d0c3
Shawn Gee <shawngee@gmail.com>
2020-02-11 23:35:24 -0500

    Added Currency Swap project

---
+++ Currency Swap/Currency Swap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Swap
+//
+//  Created by Shawn Gee on 2/11/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
