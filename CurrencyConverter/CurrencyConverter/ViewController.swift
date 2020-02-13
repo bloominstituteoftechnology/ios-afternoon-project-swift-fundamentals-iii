f441f6ffc826ee0b01b0d65f1f8e8e772abc96bf
Getu Gebre <getu.gebre.gg@gmail.com>
2020-02-13 17:17:15 -0600

    completed project

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,9 +8,22 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
+    var currency: Currency = .cad
+    let conversionDictionary = ["CAD" : 1.33, "MXN": 19.70]
+    
+    var currencyFormatter: NumberFormatter = {
+       let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
     
     @IBOutlet weak var fromCurrencyTextField: UITextField!
     @IBOutlet weak var toCurrencyTextField: UITextField!
@@ -23,17 +36,47 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        if let dollarsInput = fromCurrencyTextField.text,
+            let dollar = Double(dollarsInput),
+            let currencyFormatted = currencyFormatter.string(from: NSNumber(value: convert(dollar))){
+            
+            toCurrencyTextField.text = currencyFormatted
+        }
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        cadButton.backgroundColor = #colorLiteral(red: 0.1306751072, green: 0.5215686275, blue: 0.9667938352, alpha: 1)
+        cadButton.setTitleColor(.white, for:    .normal)
+        mxnButton.backgroundColor = .white
+        mxnButton.setTitleColor(#colorLiteral(red: 0.1306751072, green: 0.5215091109, blue: 0.9667938352, alpha: 1), for: .normal)
+        
+        mxnButton.isSelected = false
+        currency = .cad
+        toCurrencyLabel.text = "Currency (CAD)"
         
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        mxnButton.backgroundColor = #colorLiteral(red: 0.1306751072, green: 0.5215091109, blue: 0.9667938352, alpha: 1)
+        mxnButton.setTitleColor(.white, for: .normal)
+        cadButton.backgroundColor = .white
+        cadButton.setTitleColor(#colorLiteral(red: 0.1306751072, green: 0.5215091109, blue: 0.9667938352, alpha: 1), for: .normal)
         
+        cadButton.isSelected = false
+        currency = .mxn
+        toCurrencyLabel.text = "Currency (MXN)"
+      
     }
     
     // MARK: - Helper Methods
+    
+    func convert(_ dollars: Double) -> Double {
+            switch currency {
+            case .cad:
+                return conversionDictionary["CAD"]! * dollars
+            case .mxn:
+                return conversionDictionary["MXN"]! * dollars
+            }
+    }
 }
 
