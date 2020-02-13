ca1cd33a7a3c4bdf72e37a754a1b4f694e60c1e5
Shawn Gee <shawngee@gmail.com>
2020-02-12 23:19:02 -0500

    renaming to fix

---
+++ currencyswaper/CurrencySwap/Extensions/UIView+Layout.swift
@@ -0,0 +1,40 @@
+//
+//  UIView+Layout.swift
+//  Currency Swap
+//
+//  Created by Shawn Gee on 2/12/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @discardableResult
+    func centerHorizontally(multiplier: CGFloat = 1) -> UIView {
+        guard let superview = superview else { print("view has no superview center in") ; return self}
+        self.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: superview, attribute: .centerX, multiplier: multiplier, constant: 0).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func centerVertically(multiplier: CGFloat = 1) -> UIView {
+        guard let superview = superview else { print("view has no superview center in") ; return self}
+        self.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: superview, attribute: .centerY, multiplier: multiplier, constant: 0).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func fillSuperview() -> UIView {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        guard let superview = superview else { print("view has no superview to fill") ; return self}
+        NSLayoutConstraint.activate([
+            self.leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            self.topAnchor.constraint(equalTo: superview.topAnchor),
+            self.bottomAnchor.constraint(equalTo: superview.bottomAnchor),
+        ])
+        return self
+    }
+}
